76bf0f6241c26936f400c507f2513e07caaecd4a
Nirvan Adam <nirvan.pramudhyta@gmail.com>
2025-03-19 22:07:57 +0700

    add more products n create pagination feature

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -23,26 +23,38 @@ function AllProduct() {
       ? products
       : products.filter((product) => product.category === category);
 
-  let titleHeader = "";
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
 
-  if (category === "all") {
-    titleHeader = "All Products";
-  } else if (category === "tops") {
-    titleHeader = "Tops";
-  } else if (category === "pants") {
-    titleHeader = "Pants";
-  } else if (category === "shoes") {
-    titleHeader = "Shoes";
-  } else if (category === "jacket") {
-    titleHeader = "Jacket";
-  }
+  const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const displayProducts = filteredProducts.slice(
+    startIndex,
+    startIndex + itemsPerPage,
+  );
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [filteredProducts]);
+
+  useEffect(() => {
+    const productSection = document.getElementById("top");
+    if (productSection) {
+      productSection.scrollIntoView({ behavior: "auto" });
+    }
+  }, [currentPage]);
 
   return (
     <div>
-      <header className="relative mt-20 flex flex-col items-center text-center md:mt-32">
+      <header
+        id="top"
+        className="relative mt-20 flex flex-col items-center text-center md:mt-32"
+      >
         <span className="absolute -top-5 h-[2px] w-[120px] bg-black md:-top-7" />
         <h1 className="text-4xl font-medium uppercase sm:text-5xl">
-          {titleHeader}
+          {category === "all" ? "All Products" : category}
         </h1>
         <p className="text-text-secondary mt-5 font-medium md:text-lg lg:w-2/3">
           Lorem ipsum dolor sit amet consectetur adipiscing elit eu et posuere
@@ -52,8 +64,8 @@ function AllProduct() {
 
       <main className="mt-20 grid-cols-[0.8fr_3fr] lg:mt-28 lg:grid lg:gap-10">
         <section className="grid grid-cols-2 gap-3 gap-y-10 lg:order-2 lg:grid-cols-3 xl:grid-cols-4">
-          {filteredProducts.length > 0 ? (
-            filteredProducts.map((product) => (
+          {displayProducts.length > 0 ? (
+            displayProducts.map((product) => (
               <ProductCard
                 key={product.id}
                 image={product.image}
@@ -63,55 +75,85 @@ function AllProduct() {
               />
             ))
           ) : (
-            <h2 className="self-center justify-self-center">Product kosong</h2>
+            <div className="animate-pulse">
+              <div className="flex items-center justify-center overflow-hidden bg-gray-100">
+                <div className="h-[300px] w-full bg-gray-200" />
+              </div>
+
+              <h1 className="mt-5 h-[15px] w-full bg-gray-200"></h1>
+              <p className="mt-1 h-[15px] w-full bg-gray-200"></p>
+
+              <h1 className="mt-2 h-[15px] w-1/2 bg-gray-200"></h1>
+            </div>
           )}
         </section>
 
         <aside className="mt-16 lg:order-1 lg:mt-0">
-          <h1 className="border-b border-gray-300 pb-4 text-lg uppercase sm:text-xl">
+          <h1 className="border-b border-gray-300 pb-4 text-lg uppercase">
             Sort by Category
           </h1>
 
           <nav className="text-text-secondary mt-5 grid grid-cols-2 gap-3 uppercase lg:grid-cols-1">
             <Link
               href="/category/all"
-              className={`${category === "all" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3`}
+              className={`${category === "all" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3 transition hover:text-black`}
             >
               All
             </Link>
             <Link
               href="/category/tops"
-              className={`${category === "tops" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3`}
+              className={`${category === "tops" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3 transition hover:text-black`}
             >
               Tops
             </Link>
             <Link
               href="/category/pants"
-              className={`${category === "pants" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3`}
+              className={`${category === "pants" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3 transition hover:text-black`}
             >
               Pants
             </Link>
             <Link
               href="/category/shoes"
-              className={`${category === "shoes" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3`}
+              className={`${category === "shoes" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3 transition hover:text-black`}
             >
               Shoes
             </Link>
             <Link
               href="/category/jackets"
-              className={`${category === "jackets" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3`}
+              className={`${category === "jackets" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3 transition hover:text-black`}
             >
               Jacket
             </Link>
             <Link
               href="/category/hoodies"
-              className={`${category === "hoodies" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3`}
+              className={`${category === "hoodies" ? "border-black font-medium text-black" : "border-transparent"} border-l-2 pl-3 transition hover:text-black`}
             >
               Hoodie
             </Link>
           </nav>
         </aside>
       </main>
+
+      {/* Navigasi Pagination */}
+      <div className="mt-4 flex justify-center space-x-4">
+        <button
+          className="cursor-pointer rounded-md bg-gray-300 px-4 py-2"
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage((prev) => prev - 1)}
+        >
+          Prev
+        </button>
+        <span className="text-lg font-bold">
+          {currentPage} / {totalPages}
+        </span>
+        <button
+          className="cursor-pointer rounded-md bg-gray-300 px-4 py-2"
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage((prev) => prev + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomePage() {
   return (
     <main>
       {/* Hero Section */}
-      <section className="before relative -mx-5 flex h-[600px] items-center bg-[url(/images/hero.jpg)] bg-cover bg-[center] text-white lg:-mx-16 lg:h-[85vh] xl:-mx-24 2xl:-mx-64">
+      <section className="before relative -mx-5 flex h-[600px] items-center bg-[url(/images/assets/hero.jpg)] bg-cover bg-[center] text-white lg:-mx-16 lg:h-[85vh] xl:-mx-24 2xl:-mx-64">
         <span className="absolute h-full w-full bg-black opacity-70" />
 
         <div className="absolute w-full px-5 lg:w-[60%] lg:px-10 xl:w-[65%] xl:px-20 2xl:px-60">
@@ -34,32 +34,36 @@ function HomePage() {
 
           <div className="mt-10 flex flex-col gap-5 uppercase sm:flex-row md:w-[600px] lg:w-full">
             <Link
-              href="/"
-              className="flex w-full items-center justify-center bg-white px-7 py-5 text-center text-lg font-medium text-black xl:py-7"
+              href="/category/all"
+              className="flex w-full items-center justify-center bg-white px-7 py-5 text-center text-lg font-medium text-black transition duration-300 hover:-translate-y-1 xl:py-7"
             >
               Explore Products
             </Link>
-            <Link
-              href="/"
-              className="flex w-full items-center justify-center border border-white px-7 py-5 text-lg font-medium xl:py-7"
+
+            <a
+              href="#category"
+              className="flex w-full items-center justify-center border border-white px-7 py-5 text-lg font-medium transition duration-300 hover:-translate-y-1 hover:bg-white hover:text-black xl:py-7"
             >
               Categories
-            </Link>
+            </a>
           </div>
         </div>
       </section>
 
       {/* Category Menu Section */}
-      <section className="mt-24 sm:mt-32 md:mt-32">
+      <section id="category" className="mt-24 sm:mt-32 md:mt-32">
         <header className="items-center justify-between lg:flex">
           <h1 className="relative text-2xl font-medium uppercase lg:text-3xl">
             <span className="absolute -top-4 h-[2px] w-[20%] bg-black sm:w-[15%]" />
             Explore by Category
           </h1>
 
-          <button className="mt-5 flex w-full items-center justify-center border border-black px-7 py-4 text-lg font-medium uppercase sm:mt-8 sm:w-fit md:py-5 lg:mt-0">
+          <Link
+            href="/category/all"
+            className="mt-5 flex w-full items-center justify-center border border-black px-7 py-4 text-lg font-medium uppercase transition duration-300 hover:-translate-y-1 hover:bg-black hover:text-white sm:mt-8 sm:w-fit md:py-5 lg:mt-0"
+          >
             All Products
-          </button>
+          </Link>
         </header>
 
         <article className="mt-10 gap-5 sm:mt-14">
@@ -67,13 +71,13 @@ function HomePage() {
             <CategoryCard
               category="tops"
               title="Tops"
-              image="/images/tshirt_category.jpg"
+              image="/images/assets/tshirt_category.jpg"
               style="lg:aspect-video"
             />
             <CategoryCard
               category="pants"
               title="Pants"
-              image="/images/pants_category.jpg"
+              image="/images/assets/pants_category.jpg"
               style="lg:aspect-video"
             />
           </main>
@@ -82,19 +86,19 @@ function HomePage() {
             <CategoryCard
               category="shoes"
               title="Shoes"
-              image="/images/shoes_category.jpg"
+              image="/images/assets/shoes_category.jpg"
               style="col-span-2 aspect-video lg:col-span-1 lg:aspect-square lg:bg-[-50px]"
             />
             <CategoryCard
               category="jackets"
               title="Jackets"
-              image="/images/jacket_category.jpg"
+              image="/images/assets/jacket_category.jpg"
               style=""
             />
             <CategoryCard
               category="hoodies"
               title="Hoodies"
-              image="/images/hoodie_category.jpg"
+              image="/images/assets/hoodie_category.jpg"
               style=""
             />
           </main>
@@ -109,12 +113,15 @@ function HomePage() {
             Get 10% Off on Your First Order
           </h1>
 
-          <button className="mt-10 flex w-full items-center justify-center bg-white px-7 py-4 text-lg font-medium uppercase sm:w-fit md:py-5">
+          <Link
+            href="/category/all"
+            className="mt-10 flex w-full items-center justify-center bg-white px-7 py-4 text-lg font-medium uppercase sm:w-fit md:py-5"
+          >
             Explore Products
-          </button>
+          </Link>
         </div>
 
-        <div className="h-[450px] w-full bg-[url(/images/hero2.jpg)] bg-cover xl:h-full xl:bg-[center_-100px]" />
+        <div className="h-[450px] w-full bg-[url(/images/assets/hero2.jpg)] bg-cover xl:h-full xl:bg-[center_-100px]" />
       </section>
 
       {/* Latest Products Section */}
@@ -125,9 +132,12 @@ function HomePage() {
             Latest Product
           </h1>
 
-          <button className="mt-5 flex w-full items-center justify-center border border-black px-7 py-4 text-lg font-medium uppercase sm:mt-8 sm:w-fit md:py-5 lg:mt-0">
+          <Link
+            href="/category/all"
+            className="mt-5 flex w-full items-center justify-center border border-black px-7 py-4 text-lg font-medium uppercase transition duration-300 hover:-translate-y-1 hover:bg-black hover:text-white sm:mt-8 sm:w-fit md:py-5 lg:mt-0"
+          >
             All Products
-          </button>
+          </Link>
         </header>
 
         <article className="mt-10 grid grid-cols-2 gap-3 gap-y-10 lg:grid-cols-4">
