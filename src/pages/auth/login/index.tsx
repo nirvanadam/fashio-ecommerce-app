e89640dd638813f618289ae64a847141c490890b
Nirvan Adam <nirvan.pramudhyta@gmail.com>
2025-03-24 13:32:41 +0700

    update ui login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -5,16 +5,24 @@ import React from "react";
 
 export default function LoginPage() {
   return (
-    <div className="-mx-5 h-screen bg-white text-black lg:-mx-16 xl:-mx-24 2xl:-mx-64">
-      <header className="px-7 py-10 pt-32">
-        <Link href="/" className="absolute top-10 left-0 bg-black px-3 py-2">
+    <div className="-mx-5 h-screen bg-white text-black lg:-mx-16 lg:grid lg:grid-cols-2 xl:-mx-24 2xl:-mx-64">
+      <header className="px-7 py-10 pt-32 sm:px-16 md:px-28 lg:content-center lg:justify-items-center lg:bg-black lg:px-0 lg:py-0 lg:text-white">
+        <Link
+          href="/"
+          className="absolute top-10 left-0 bg-black px-3 py-2 lg:hidden"
+        >
           <ChevronLeft color="#fff" className="" size={32} />
         </Link>
-        <h1 className="text-4xl font-medium">Login In</h1>
+        <h1 className="text-4xl font-semibold lg:hidden">Login</h1>
+        <h1 className="hidden text-5xl font-medium tracking-widest lg:block">
+          FASHIO
+        </h1>
       </header>
 
-      <main className="px-7">
-        <form action="" className="flex flex-col gap-5">
+      <main className="px-7 sm:px-16 md:px-28 lg:content-center lg:px-16 xl:px-28 2xl:px-52">
+        <h1 className="hidden text-4xl font-semibold lg:block">Login</h1>
+
+        <form action="" className="flex flex-col gap-5 lg:mt-10">
           <div className="flex flex-col gap-1">
             <label htmlFor="email" className="font-medium">
               Email
@@ -59,14 +67,14 @@ export default function LoginPage() {
           </button>
         </form>
 
-        <div className="relative mt-10 h-[1px] w-full bg-neutral-600">
-          <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3 text-sm font-medium">
+        <div className="relative mt-10 h-[1px] w-full bg-neutral-300">
+          <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white px-3 text-sm font-medium text-gray-500">
             OR
           </h1>
         </div>
 
         <div className="mt-10 flex flex-col">
-          <button className="relative flex w-full justify-center gap-3 border border-black p-3">
+          <button className="relative flex w-full justify-center gap-3 border-2 border-neutral-300 p-3">
             <Image
               src="/images/logo/google.svg"
               width={24}
@@ -74,7 +82,7 @@ export default function LoginPage() {
               alt=""
               className="absolute top-1/2 left-3 -translate-y-1/2"
             />
-            <h1 className="font-medium">Continue with Google</h1>
+            <h1 className="font-semibold">Continue with Google</h1>
           </button>
         </div>
       </main>
